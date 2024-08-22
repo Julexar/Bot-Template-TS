b36acc131ba8e5eec56138ec38c9fb7a526b24ef
Julexar <julexar@gmail.com>
2024-08-22 08:24:20 +0000

    Little fix
    Signed-off-by: Julexar <julexar@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { DiscordClient } from './client/lib/DiscordClient';
 
-export const client = new DiscordClient();
+const client = new DiscordClient();
 
 client.start();
 
 client.console.info('Client started!')
+
+export { client };
